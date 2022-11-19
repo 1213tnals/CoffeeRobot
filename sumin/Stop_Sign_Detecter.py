8824c34d21017c7723d1d7c1539106af25dd5124
callme60 <108113533+callme60@users.noreply.github.com>
2022-11-19 16:03:13 +0900

    Update Stop_Sign_Detecter.py

--- sumin/Stop_Sign_Detecter.py
+++ sumin/Stop_Sign_Detecter.py
@@ -53,11 +53,11 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 @smart_inference_mode()
 def run(
-        weights=ROOT / 'yolov5s.pt',  # model path or triton URL
-        source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
+        weights=ROOT / 'stop_sign.pt',  # model path or triton URL
+        source=ROOT / '0',  # file/dir/URL/glob/screen/0(webcam)
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(360, 360),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
+        conf_thres=0.7,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
